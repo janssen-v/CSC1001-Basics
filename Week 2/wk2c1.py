dfafd0fc31d46ebcb80e745f9ef65e9512016c92
janssen-v <esper@Spectre.localdomain>
2020-01-16 11:19:39 +0800

    added lecture practice

--- Week 2/wk2c1.py
+++ Week 2/wk2c1.py
@@ -1,2 +1,10 @@
 # Week 2 Class 2
 # No need to define datatypes in Python, will automatically do for you
+# Use all caps to define a variable-constant. E.g. SEC_PER_MIN = 60
+# Always define a variable-constant and use it, do not do direct calculations
+# Input function returns a string always
+# Practice -> Write a program to read a user's weight and height and then output his BMI
+
+# BMI Calculator
+BMI = (weight / pow(height,2))
+input()
