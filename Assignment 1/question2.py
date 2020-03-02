eaa23b7cc21256267b125e75f82f1217d09f8545
janssen-v <janssen.vincentius@gmail.com>
2020-03-03 01:25:31 +0700

    working but doesnt autorepeat on failure

--- Assignment 1/question2.py
+++ Assignment 1/question2.py
@@ -2,14 +2,27 @@
 
 #Asks the user to input an integer
 num = (input("Enter an integer: "))
-
-#Header for vertical list
-print ("The integer is: ")
-
-#Converts input into a horizontal list
-horz = list(num)
-
-#This prints the list vertically
-for i in range(len(horz)):
-    print(horz[i], end ='''
+#Checks if input is an integer
+try:
+    val = int(num)
+    #Converts input into a list
+    horz = list(num)
+    #Header for vertical list
+    print ("The integer is: ")
+    #Prints list vertically
+    for i in range(len(horz)):
+        print(horz[i], end ='''
 ''')
+except ValueError:
+    print("You did not input an integer, please try again")
+
+
+
+
+
+
+
+
+
+
+
