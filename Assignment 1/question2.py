79db55bb49ba3e795d1c9d3412a8acd177aa3538
janssen-v <janssen.vincentius@gmail.com>
2020-03-03 01:40:47 +0700

    code

--- Assignment 1/question2.py
+++ Assignment 1/question2.py
@@ -1,20 +1,24 @@
 #Question 2
 
-#Asks the user to input an integer
-num = (input("Enter an integer: "))
-#Checks if input is an integer
-try:
-    val = int(num)
-    #Converts input into a list
-    horz = list(num)
-    #Header for vertical list
-    print ("The integer is: ")
-    #Prints list vertically
-    for i in range(len(horz)):
-        print(horz[i], end ='''
+#Loops statements until integer is input
+done = False
+while not done:
+    try:
+        #Asks the user to input an integer
+        num = (input("Enter an integer: "))
+        #Checks if input is an integer
+        int(num)
+        #Converts input into a list
+        horz = list(num)
+        #Header for vertical list
+        print ("The integer is: ")
+        #Prints list vertically
+        for i in range(len(horz)):
+           print(horz[i], end ='''
 ''')
-except ValueError:
-    print("You did not input an integer, please try again")
+        done = True
+    except ValueError:
+        print("You did not input an integer, please try again")
 
 
 
