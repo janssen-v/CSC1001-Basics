d5cb8c7694be67b640ba0317421ff0c4d8f9b3fc
janssen-v <janssen.vincentius@gmail.com>
2020-03-03 01:53:49 +0700

    formatting change

--- Assignment 1/question1.py
+++ Assignment 1/question1.py
@@ -3,4 +3,4 @@ finalAccountValue       =   float(input("Enter Final Account Value: "))
 annualInterestRate      =   (float(input("Enter Annual Interest Rate: ")))/100
 numberOfYears           =   int(input("Enter Number of Years: "))
 initialDepositAmount    =   finalAccountValue/((1+annualInterestRate)**numberOfYears)
-print   ("The initial deposit amount should be:", initialDepositAmount)
+print ("The initial deposit amount should be:", initialDepositAmount)
