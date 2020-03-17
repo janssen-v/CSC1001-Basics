712085a35626bc665c2a59e2630f2d43757580fa
janssen-v <janssen.vincentius@gmail.com>
2020-03-18 00:21:17 +0700

    finished with q2 spec

---
+++ Assignment 2/question2.py
@@ -0,0 +1,9 @@
+# Question 2
+# Display the Emirps
+# Program Flow
+# 0. Enumerator
+# 1. Check if Palindrome
+# 2. Check if Prime
+# 3. If Prime && Palindrome, add to list
+# 4. When list has 100 items, print
+
