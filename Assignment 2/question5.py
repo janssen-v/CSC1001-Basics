f2337dfcaeed4ae03f7bc2766dc95226dd9284b2
janssen-v <janssen.vincentius@gmail.com>
2020-03-24 15:46:26 +0700

    Removed function as numlocker constant

--- Assignment 2/question5.py
+++ Assignment 2/question5.py
@@ -1,13 +1,11 @@
 # Locker Puzzle
-def locker_puzzle(num):
-    lockerState = [False]*num
+lockerState = [False]*100
 # Starts on every next locker with every student, the increment
 # of lockerstate changes increasing with student count as well.
-    for students in range(1, num+1):
-        for lock in range(students, num+1,students):
-            lockerState[lock-1] = not lockerState[lock-1]  
-    return lockerState
+for students in range(1, 101):
+    for lock in range(students, 101,students):
+        lockerState[lock-1] = not lockerState[lock-1]  
 
-for state,open in enumerate(locker_puzzle(100)):
+for state,open in enumerate(lockerState):
     if open:
         print('Locker', state+1) # Because starts from zero
