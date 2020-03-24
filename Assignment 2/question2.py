34f1dabcf3700ed94594af0c071cd744cc44d1cc
janssen-v <janssen.vincentius@gmail.com>
2020-03-24 16:50:55 +0700

    formatting fix

--- Assignment 2/question2.py
+++ Assignment 2/question2.py
@@ -1,12 +1,4 @@
-# Question 2
 # Display the Emirps
-# Program Flow
-# 0. Enumerator
-# 1. Check if Palindrome
-# 2. Check if Prime
-# 3. If Prime && Palindrome, add to list
-# 4. When list has 100 items, print
-
 palinPrime = []
 candidatePrime = 1
 totalFound = 0
--- Assignment 2/question4.py
+++ Assignment 2/question4.py
@@ -1,9 +1,9 @@
 # Anagram Checker
 def isAnagram(s1, s2):
     if sorted(s1) == sorted(s2):
-        print(s1, 'and', s2, 'are anagrams.')
+        print(s1, 'and', s2, 'is an anagram.')
     else:
-        print(s1, 'and', s2, 'are not anagrams.')
+        print(s1, 'and', s2, 'is not an anagram.')
 
 print('This program checks if two words are anagrams')
 print()
