2ed424594e5836281f6ac886e2a7dffc3537642a
janssen-v <janssen.vincentius@gmail.com>
2020-03-24 15:45:29 +0700

    Q5 Complete

--- Assignment 2/question5.py
+++ Assignment 2/question5.py
@@ -1,1 +1,13 @@
 # Locker Puzzle
+def locker_puzzle(num):
+    lockerState = [False]*num
+# Starts on every next locker with every student, the increment
+# of lockerstate changes increasing with student count as well.
+    for students in range(1, num+1):
+        for lock in range(students, num+1,students):
+            lockerState[lock-1] = not lockerState[lock-1]  
+    return lockerState
+
+for state,open in enumerate(locker_puzzle(100)):
+    if open:
+        print('Locker', state+1) # Because starts from zero
