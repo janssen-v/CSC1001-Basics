39499c8293fe2c232c57b17cec744d367aa3e6b6
janssen-v <janssen.vincentius@gmail.com>
2020-05-03 02:31:29 +0700

    A4Q3 Complete

---
+++ Assignment 4/q3.py
@@ -0,0 +1,64 @@
+# Question 3 - Tower of Hanoi (Iterative)
+# Note: It is much easier to do the Tower of Hanoi with recursion
+
+def move (origin, dest): # Move from origin to destination
+    dest.insert(0, origin.pop(0))
+
+def legalMove(tower1, tower2, name1, name2):
+    if len(tower2) == 0 and len(tower1) != 0: # if tower 2 is empty and tower 1 is not
+        move(tower1, tower2)
+        print(name1, '-->', name2)
+    elif len(tower1) == 0 and len(tower2) != 0: # if tower 1 is empty and tower 2 is not
+        move(tower2, tower1)
+        print(name2, '-->', name1)
+    elif len(tower1) >=0 and len(tower2) >= 0: # if both towers have a disc inside
+        if tower1[0] > tower2[0]:
+            move(tower2, tower1)
+            print(name2, '-->', name1)
+        elif tower2[0] > tower1[0]:
+            move(tower1, tower2)
+            print(name1, '-->', name2)
+
+def HanoiTower(n):
+    towA = []
+    towB = []
+    towC = []
+    towA.extend(range(1, n+1))
+    
+    if n%2 == 0: # if even
+        while True:
+            # Move AB
+            try: legalMove(towA, towB, 'A', 'B')
+            except: break
+            # Move AC
+            try: legalMove(towA, towC, 'A', 'C')
+            except: break
+            # Move BC
+            try: legalMove(towB, towC, 'B', 'C')
+            except: break
+                
+    elif n%2 != 0: # if odd         
+        while True:    
+            # Move AC
+            try: legalMove(towA, towC, 'A', 'C')
+            except: break     
+            # Move AB
+            try: legalMove(towA, towB, 'A', 'B')
+            except: break
+            # Move BC
+            try: legalMove(towB, towC, 'B', 'C')
+            except: break
+                
+if __name__ == "__main__":
+    print('Welcome to the TOWER OF HANOI SOLUTION PRINTER')
+    while True:
+        try:
+            n = int(input('Please input n amount of disks to calculate for: '))
+            assert n > 0
+        except ValueError:
+            print('Sorry, n must be an integer')
+        except AssertionError:
+            print("Sorry, I should've said that n must also be greater than 0")
+        else:
+            break
+    HanoiTower(n)
