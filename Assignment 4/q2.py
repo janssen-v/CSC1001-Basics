00423a9f3a6bb2776c0501c5d1c65e587413d754
janssen-v <janssen.vincentius@gmail.com>
2020-05-03 00:04:20 +0700

    A4Q2 initialized

---
+++ Assignment 4/q2.py
@@ -0,0 +1,6 @@
+# Question 2 - Quick sort over singly linked list
+
+class Node(object):
+    def __init__(self, data = None, nextNode = None):
+      self.data = data # List head
+      self.nextNode = nextNode
